e520392b3b0f48fe6273e2ed98dc9c179aac11f6
Khairunnisa <khrnsa143@gmail.com>
2026-02-25 04:46:14 +0000

    update index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ function hello(nama: string, umur: number): string {
     return 'Selamat Datang ' + nama + ' di PWL, umur ' + nama + ' adalah ' + umur;
 }
 
-const mahasiswa = "Soob";
+const mahasiswa = "Khairunnisa";
 const umur: number = 19;
 console.log(hello(mahasiswa, umur));
